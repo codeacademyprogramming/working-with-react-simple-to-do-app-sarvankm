bc14331aa7a102022c7b5bb95395ba727097d7e9
sarvankm <sarvankm@code.edu.az>
2021-07-01 12:41:07 +0400

    create is ok

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,56 @@
 import { useState } from "react";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { ListGroup } from "react-bootstrap";
 import "./Todo.css";
 
 export const Todo = () => {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
 
-    return (
-        <>
-            <h1>To do app</h1>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Form.Control type="text" placeholder="enter a todo" className="custom-input" />
-                <Button style={{ flex: 1, whiteSpace: 'nowrap', marginLeft: '16px' }}>Add todo</Button>
-            </div>
-        </>
-    )
-}
+  const myArrMadeFromForEach = [];
+
+  const pushTitle = () => {
+    setTodos([...todos, title]);
+  };
+  let count = 0;
+  todos.forEach((val) => {
+    count++;
+    myArrMadeFromForEach.push(
+      <ListGroup.Item key={count} id={count}>
+        {val}
+        <p style={{ textAlign: "right" }}>x</p>
+      </ListGroup.Item>
+    );
+  });
+  return (
+    <>
+      <h1>To do app</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Form.Control
+          type="text"
+          placeholder="enter a todo"
+          className="custom-input"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <Button
+          style={{ flex: 1, whiteSpace: "nowrap", marginLeft: "16px" }}
+          onClick={pushTitle}
+        >
+          Add todo
+        </Button>
+      </div>
+      <ListGroup style={{ display: "inline-block", width: "79.8%" }}>
+        <div style={{ marginTop: "10px" }}>{myArrMadeFromForEach}</div>
+      </ListGroup>
+    </>
+  );
+};
